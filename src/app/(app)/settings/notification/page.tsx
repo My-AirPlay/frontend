d1f15001493d03dc1f785543de8c1cc06a968d25
gideon-del <gideonchidi471@gmail.com>
2025-01-21 12:01:36 +0100

    chore: done with user profile screens

--- src/app/(app)/settings/notification/page.tsx
+++ src/app/(app)/settings/notification/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SettingsNav from "../_components/settings-nav/settings-nav";
+import SettingsProfile from "../_components/settings-profile/settings-profile";
 
 const Notifications = () => {
   return (
-    <div>
+    <SettingsProfile>
       <SettingsNav />
-    </div>
+    </SettingsProfile>
   );
 };
 
